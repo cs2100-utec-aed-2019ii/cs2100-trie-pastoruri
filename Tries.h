135e4d9380c636a93df2fc6227cb421d5c75b0f4
pastoruri <40170714+pastoruri@users.noreply.github.com>
2019-12-02 08:09:26 -0500

    feat(4/5 exercises) : 4 out of 5 exercises implemented.

--- Tries.h
+++ Tries.h
@@ -3,21 +3,199 @@
 #include <map>
 #include <string>
 #include <queue>
+#include <random>
+#include <ctime>
 using namespace std;
 
 struct Node{
     bool is_word = false;
-    map<char,Node*> characters;
+    map<string,Node*> characters;
     Node() = default;
     ~Node() = default;
+    Node* next = nullptr;
+    Node* parent = nullptr;
+    Node* previous = nullptr;
+    int colisiones = 0;
 };
 
 
 struct Trie{
     Node* root = new Node;
+    Node* lastfinal = nullptr;
+    int cant_words_totales = 0;
+    int cant_words_unique = 0;
+    Node* lasttail = nullptr;
+    float avg_frequency(){
+        return cant_words_totales / float(cant_words_unique);
+    }
 
     Trie() = default;
+    vector<string> ejercicio5(string word){
+        vector<string> palabras;
+        auto  aux = lastfinal;
+        auto track = lastfinal;
+        while(aux != nullptr){
+            bool col = true;
+            string pal;
+            palabras.push_back(pal);
+            int t = palabras.size()-1;
+            while(aux != nullptr) {
+                auto padre = aux->parent;
+                if(padre!=nullptr){
+                    for (auto it = padre->characters.begin(); it != padre->characters.end(); ++it) {
+                        if (it->second == aux) {
+                            palabras[t]+=it->first;
+                        }
+                    }
+                }
+                aux = aux->parent;
+            }
+            aux = track->next;
+            track = track->next;
+        }
 
+        for(int i=0 ; i< palabras.size(); ++i){
+            palabras[i] = reverse(palabras[i]);
+        }
+        vector<string> matches;
+
+        for(int i=0 ; i < palabras.size(); ++i){
+            int count = 0;
+            for(int j=0; j < palabras[i].size(); ++j){
+                 if(word[count] == palabras[i][j]){
+                     ++count;
+                    if(count == word.size()){
+                        matches.push_back(palabras[i]);
+                        cout << palabras[i] << endl;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        return matches;
+
+
+
+
+    }
+
+
+
+    void ejercicio2(){
+        cout << "Cuantas filas tendra la matriz: " << endl;
+        int filas;
+        cin >> filas;
+        for(int i=0; i < filas; ++i){
+           cout << "Ingrese fila " << i << endl;
+           string c;
+           cin >> c;
+           insert(c);
+        }
+        cout << "Filas que se repiten al menos una vez: " << endl;
+        auto  aux = lastfinal;
+        auto track = lastfinal;
+        while(aux != nullptr){
+            bool col = true;
+            auto initial_node = aux;
+            while(aux != nullptr) {
+                if(initial_node->is_word && initial_node->colisiones == 0 ){
+                    break;
+                }
+                else{
+                    auto padre = aux->parent;
+                    if(aux->is_word == true && col){
+                        col = false;
+                        cout << "Repetidas: " << aux->colisiones << " veces." << endl;
+                    }
+                    if(padre!=nullptr){
+                        for (auto it = padre->characters.begin(); it != padre->characters.end(); ++it) {
+                            if (it->second == aux) {
+                                cout << it->first << endl;
+                            }
+                        }
+                    }
+                    aux = aux->parent;
+                }
+            }
+            aux = track->next;
+            track = track->next;
+            cout << endl;
+        }
+
+
+    }
+
+
+    void ejercicio3(){
+        cout << "Palabras con frecuencia mayor que la media: " << endl;
+
+        auto  aux = lastfinal;
+        auto track = lastfinal;
+        while(aux != nullptr){
+            bool col = true;
+            auto initial_node = aux;
+            while(aux != nullptr) {
+                if(initial_node->is_word && initial_node->colisiones+1 < avg_frequency() ){
+                    break;
+                }
+                else{
+                    auto padre = aux->parent;
+                    if(aux->is_word == true && col){
+                        col = false;
+                        cout << "Frecuencia: " << aux->colisiones+1 << endl;
+                    }
+                    if(padre!=nullptr){
+                        for (auto it = padre->characters.begin(); it != padre->characters.end(); ++it) {
+                            if (it->second == aux) {
+                                cout << it->first << endl;
+                            }
+                        }
+                    }
+                    aux = aux->parent;
+                }
+            }
+            aux = track->next;
+            track = track->next;
+            cout << endl;
+        }
+    }
+    string reverse(string const &s)
+    {
+        string rev;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            rev = rev.append(1, s[i]);
+        };
+
+        return rev;
+    }
+
+    void print_all_words(){
+        auto  aux = lastfinal;
+        auto track = lastfinal;
+        while(aux != nullptr){
+            bool col = true;
+            while(aux != nullptr) {
+                auto padre = aux->parent;
+                if(aux->is_word == true && col){
+                    col = false;
+                    cout << "Colisiones: " << aux->colisiones << endl;
+                }
+                if(padre!=nullptr){
+                    for (auto it = padre->characters.begin(); it != padre->characters.end(); ++it) {
+                        if (it->second == aux) {
+                            cout << it->first << endl;
+                        }
+                    }
+                }
+                aux = aux->parent;
+            }
+            aux = track->next;
+            track = track->next;
+            cout << endl;
+        }
+    }
     void print() {
         queue<Node*> q;
 
@@ -38,6 +216,8 @@ struct Trie{
                         q.push(child);
                     }
                 }
+                cout << endl;
+
                 q.pop();
 
                 --nodos;
@@ -48,29 +228,32 @@ struct Trie{
 
 
     void insert(string word){
-        if(root->characters.size() == 0){
-            auto aux = root;
-            for(int i=0; i<word.size(); ++i){
-                pair aux_pair(word[i], new Node);
+        auto aux = root;
+
+        for(int i=0; i<word.size(); i++) {
+            string s(1,word[i]);
+            if(!search_in_node(s,aux->characters)){
+                pair aux_pair(s, new Node);
+                auto aux_parent = aux;
                 aux->characters.insert(aux_pair);
-                aux = aux->characters[word[i]];
+                aux = aux->characters[s];
+                aux->parent = aux_parent;
             }
+            else{
+                aux = aux->characters[s];
+            }
+        }
+        if( aux->is_word == false){
             aux->is_word = true;
+            auto auxf = lastfinal;
+            aux->next = auxf;
+            lastfinal = aux;
+            cant_words_totales++;
+            cant_words_unique++;
         }
         else{
-            auto aux = root;
-            int track = 0;
-            for(int i=0; i<word.size(); i++) {
-                if(!search_in_node(word[i],aux->characters)){
-                    pair aux_pair(word[i], new Node);
-                    aux->characters.insert(aux_pair);
-                    aux = aux->characters[word[i]];
-                }
-                else{
-                    aux = aux->characters[word[i]];
-                }
-            }
-            aux->is_word = true;
+            aux->colisiones++;
+            cant_words_totales++;
         }
     }
 
@@ -79,7 +262,8 @@ struct Trie{
         bool fail_case = false;
         for(int i = 0; i<word.size(); ++i){
             for (auto it = aux->characters.begin(); it!= aux->characters.end(); ++it){
-                if (word[i] == it->first){
+                string s(1,word[i]);
+                if (s == it->first){
                     aux = aux->characters[it->first];
                     goto Here;
                 }
@@ -96,13 +280,69 @@ struct Trie{
         }
     };
 
+
+//    Trie patriciafy(Node* node){
+//        auto aux = node;
+//            for(auto it = aux->characters.begin(); it!=  aux->characters.end(); ++it){
+//
+//            }
+//    }
+
+    void ejercicio1(int cant_ips){
+        srand(time(NULL));
+    for(int i=0; i <(cant_ips+1); ++i){
+        string num = "192.168.";
+        int num1 = rand()%255;
+        int num2 = rand()%255;
+        num += to_string(num1);
+        num += ".";
+        num += to_string(num2);
+        insert(num);
+    }
+    string ip;
+        auto  aux = lastfinal;
+        auto track = lastfinal;
+        while(aux != nullptr){
+            while(aux != nullptr) {
+                auto padre = aux->parent;
+                if(aux->is_word == true){
+                    cout << "Existe esta ip(se lee de abajo hacia arriba): "  << endl;
+                }
+                if(padre!=nullptr){
+                    for (auto it = padre->characters.begin(); it != padre->characters.end(); ++it) {
+                        if (it->second == aux) {
+                            cout << it->first << endl;
+                        }
+                    }
+                }
+                aux = aux->parent;
+
+            }
+            aux = track->next;
+            track = track->next;
+            cout << endl;
+            break;
+        }
+
+    cout << "Ingrese ip para ser buscada: " ;
+    cin >> ip;
+        if(search_complete_word(ip)){
+            cout << endl << "Ip encontrada." <<endl;
+        }
+        else{
+            cout << endl << "Ip no encontrada." <<endl;
+        }
+
+    }
+
     bool search_by_prefix(string word){
         auto aux = root;
         bool fail_case = false;
         for(int i = 0; i<word.size(); ++i){
 
             for (auto it = aux->characters.begin(); it!= aux->characters.end(); ++it){
-                if (word[i] == it->first){
+                string s(1,word[i]);
+                if (s == it->first){
                     aux = aux->characters[it->first];
                     goto Here;
                 }
@@ -122,7 +362,7 @@ struct Trie{
 
     }
 
-    bool search_in_node(char c, map<char,Node*> characters){
+    bool search_in_node(string c, map<string,Node*> characters){
         if(characters.size() > 0){
             for(auto it = characters.begin(); it!=characters.end(); ++it){
                 if(c == it->first){
--- main.cpp
+++ main.cpp
@@ -3,20 +3,22 @@
 
 int main() {
     Trie t;
+    //t.ejercicio1(200); //le pasas la cantidad de ips a generar y te bota una ip de ejemplo para que pruebes el comando.
+    //t.ejercicio2(); // insertas fila tras fila de cadenas de 0's y 1's y te dice las repetidas
+    // t.ejercicio3(); // se inserta las palabras con t.insert y se ejecuta esta función, te devolvera las palabras con frecuencia mayor que la media.
 
     t.insert("hola");
-
-    t.insert("he");
-
-    t.insert("alo");
-
-    if(t.search_complete_word("hola")){
-        cout << "Found!" << endl;
-    }
+    t.insert("hola");
+    t.insert("homero");
+    t.insert("aaossss");
+    t.insert("ashjdhjo");
+    t.insert("hol");
+    t.insert("sim");
 
 
-    if(t.search_by_prefix("ho")){
-        cout << "Found!" << endl;
-    }
+   // t.ejercicio5("ho"); Una vez insertadas varias palabras, le pasas otra palabra a esta funcion y te devolvera las palabras en el diccionario que tienen las letras de la palabra pasada en el mismo orden en cualquier configuración.
+
+
+
     return 0;
 }
